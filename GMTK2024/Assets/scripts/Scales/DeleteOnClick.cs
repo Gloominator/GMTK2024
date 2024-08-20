cfde5880036a08995f029583625997f2dc87c5eb
Gloominator <73242439+Gloominator@users.noreply.github.com>
2024-08-20 18:59:40 +0300

    feathe bug fix hopefully

--- GMTK2024/Assets/scripts/Scales/DeleteOnClick.cs
+++ GMTK2024/Assets/scripts/Scales/DeleteOnClick.cs
@@ -6,10 +6,25 @@ public class DeleteOnClick : MonoBehaviour
 {
     public float myWeight;
     public SpawnTestWeights.SpawnPlace mySpawnPlace;
+
+    public Sprite featherSprite;
     // Start is called before the first frame update
 
 
-    
+    //on create object, check if it has feather sprite, then after 2 sec change its spriterenderer layer to -2 (bugfix maybe)
+    void Start()
+    {
+
+        if (GetComponent<SpriteRenderer>().sprite == featherSprite)
+        {
+            StartCoroutine(AfterSecHideFeather(2));
+        }
+    }
+    IEnumerator AfterSecHideFeather(float time)
+    {
+        yield return new WaitForSeconds(time);
+        GetComponent<SpriteRenderer>().sortingOrder = -2;
+    }
 
     public void SetWeightAndSpawnPlace(float weight,  SpawnTestWeights.SpawnPlace spawnPlace)
     {
